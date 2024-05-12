6730ec46dadfc72adf7aea60a1d8956326d8f67c
IshratMishu <ishratjahan0425@gmail.com>
2024-05-12 15:13:28 +0600

    dropdown menu is added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../components/Hooks/useAuth";
 import logo from '../../assets/Blue_Modern_Domain_Registrar_Business_Company_Logo-removebg-preview.png';
+import { FaChevronDown } from "react-icons/fa";
 
 const Navbar = () => {
     const { logOut, user } = useAuth();
 
     const linkStyle = ({ isActive }) => {
         return {
-            fontWeight: isActive ? "bold" : "",
-            color: isActive ? "" : "",
-            background: isActive ? "none" : "none",
-            fontSize: isActive ? "18px" : "18px"
+            fontWeight: isActive ? "medium" : "",
+            color: isActive ? "#F4CE14" : "#495E57",
+            background: isActive ? "#495E57" : "none",
+            fontSize: isActive ? "" : "16px",
+            padding: isActive? "10px": "",
         };
     };
 
@@ -18,19 +20,18 @@ const Navbar = () => {
     const links = <>
         <li><NavLink to='/' style={linkStyle}>Home</NavLink></li>
         <li><NavLink to='/allService' style={linkStyle}>Services</NavLink></li>
-        <li>
-            <details>
-                <summary>
-                    Dashboard
-                </summary>
-                <ul className="p-2 bg-base-100 rounded-t-none">
+        <ul className="dropDownMenu">
+            <li>
+                <p className="flex items-center">Dashboard<FaChevronDown className="text-xl text-[#495E57]"/></p>
+                <ul id='submenu' className="shadow-md">
                     <li><NavLink to='/addService' style={linkStyle}>Add Service</NavLink></li>
                     <li><NavLink to='/manageService' style={linkStyle}>Manage Service</NavLink></li>
                     <li><NavLink to='/bookedService' style={linkStyle}>Booked-Services</NavLink></li>
                     <li><NavLink to='/toDo' style={linkStyle}>Service-To-Do</NavLink></li>
                 </ul>
-            </details>
-        </li>
+            </li>
+        </ul>
+
     </>
 
     const link2 = <>
@@ -39,10 +40,8 @@ const Navbar = () => {
     </>
 
 
-
-
     return (
-          <div className="navbar fixed bg-base-100 left-0 top-0 z-10 p-2">
+        <div className="navbar fixed bg-base-100 left-0 top-0 z-10 p-2">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -57,9 +56,9 @@ const Navbar = () => {
                         }
                     </ul>
                 </div>
-               
-                 <Link to='/'><img className="h-12 md:h-16 lg:ml-3 ml-0" src={logo} alt="logo" /></Link>
-                
+
+                <Link to='/'><img className="h-12 md:h-16 lg:ml-3 ml-0" src={logo} alt="logo" /></Link>
+
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -73,8 +72,8 @@ const Navbar = () => {
             </div>
 
             <div className="navbar-end flex gap-2 ">
-        
-          <label className="cursor-pointer grid place-items-center">
+
+                <label className="cursor-pointer grid place-items-center">
                     <input type="checkbox" value="dark" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
                     <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
                     <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
