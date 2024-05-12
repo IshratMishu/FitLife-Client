84b77dc502df194570a173ae389698c5614ee1e1
IshratMishu <ishratjahan0425@gmail.com>
2024-05-12 16:03:43 +0600

    error page successful

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,17 @@
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
-
+import error from '../assets/error.jpg'
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-cover" style={{backgroundImage: `url(${error})`}}>
              <Helmet>
                 <title>404! Error - FlexiCare</title>
             </Helmet>
-            <div className="font-poppins text-center p-5 m-10 md:p-20 md:m-20 space-y-5">
-            <h2 className="font-bold text-6xl">Oops!</h2>
-            <p className="font-bold text-xl"> Page Not Found☹️</p>
-            <Link to='/'><button className="btn bg-[#81c784] text-black font-bold text-lg mt-5">Go Back to Home</button></Link>
+            <div className="font-poppins text-center space-y-5">
+            <h2 className="font-bold text-white text-6xl pt-28">WHOOPS!</h2>
+            <p className="font-bold text-[#F4CE14] text-9xl"> 404</p>
+            <Link to='/'><button className="btn bg-[#F4CE14] font-bold text-lg mt-5">Go Back to Home</button></Link>
         </div>
         </div>
     );
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,17 +7,18 @@ const Banner = () => {
     return (
         <div className="relative h-[350px] md:h-[600px] mt-16 text-white">
             <video autoPlay loop muted className="absolute object-cover w-full h-full">
-                <source src={video33} type="video/mp4"/>
+                <source src={video33} type="video/mp4" />
             </video>
             <Swiper className="mySwiper">
-                        <div className="p-10 md:p-36 mx-auto lg:mx-16">
-                            <p className="text-[20px] lg:text-[40px] md:text-[32px] font-bold text-center">
-                            Your Journey to Optimal Health Starts Here
-                            </p>
-                            <div className="flex justify-center">
-                                <Link to='/'><button className="bg-[#495E57] py-2 mt-5 px-3 rounded text-[#F5F7F8] font-semibold hover:bg-[#F4CE14]">Book Now</button></Link>
-                            </div>
-                        </div>              
+                <div className="p-10 md:p-36 mx-auto lg:mx-16">
+                    <p className="text-[20px] lg:text-[40px] md:text-[32px] font-bold text-center">
+                        Your Journey to Optimal <br /> Health Starts Here
+                    </p>
+                    <p className="text-sm text-balance text-center"> At FlexiCare, we are dedicated to guiding you towards optimal health, one step at a time. Whether you are aiming to sculpt your physique, boost your endurance or find balance in your mind and body.</p>
+                    <div className="flex justify-center">
+                        <Link to='/allServices'><button className="bg-[#495E57] py-2 mt-5 px-3 rounded text-[#F5F7F8] font-semibold hover:bg-[#F4CE14]">Transform Today</button></Link>
+                    </div>
+                </div>
             </Swiper>
         </div>
     );
