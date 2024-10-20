57a444cf621239d14cadfd3a78c589afd802e32c
IshratMishu <ishratjahan0425@gmail.com>
2024-10-20 18:50:49 +0600

    some issues fixed

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -9,36 +9,39 @@ const Root = () => {
     const [showLoading, setShowLoading] = useState(true);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        setTimeout(() => {
             setShowLoading(false);
         }, 2000);
-
-        return () => clearTimeout(timer);
     }, []);
-
-    if (showLoading) {
-        return (
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                fontSize: '40px',
-                color: '#495E57',
-                fontWeight: 'bold'
-            }}>
-                FitLife is Loading <span className="loading loading-ring loading-lg"></span>  <span className="loading loading-ring loading-md"></span> <span className="loading loading-ring loading-sm"></span>
-            </div>
-        );
-    }
 
     return (
         <div className='font-poppins'>
-            <Navbar></Navbar>
-            <ToastContainer />
-            <ScrollRestoration />
-            <Outlet></Outlet>
-            <Footer></Footer>
+            {showLoading ? (
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    fontSize: '40px',
+                    color: '#495E57',
+                    fontWeight: 'bold',
+                }}>
+                    FitLife is Loading
+                    <div style={{ display: 'flex', gap: '5px' }}>
+                        <span className="loading loading-ring loading-lg"></span>
+                        <span className="loading loading-ring loading-md"></span>
+                        <span className="loading loading-ring loading-sm"></span>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <Navbar />
+                    <ToastContainer />
+                    <ScrollRestoration />
+                    <Outlet />
+                    <Footer />
+                </div>
+            )}
         </div>
     );
 };
